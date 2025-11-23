13e9f59997accf4a731e7e5151cad8ba9d7d69e5
Salepate <salepate@gmail.com>
2025-11-23 18:48:52 +0100

    add missing data

--- EOAP.Plugin/EO1.cs
+++ EOAP.Plugin/EO1.cs
@@ -4,10 +4,25 @@ namespace EOAP.Plugin
 {
     public static class EO1
     {
+        public const string WorldName = "Etrian Odyssey HD";
         // DB
         public static Dictionary<int, string> ItemIDToName = new Dictionary<int, string>()
         {
-            { 1, "Knife" }
+            { 1, "Knife" },
+        };
+
+        public static Dictionary<uint, string> TreasureBox = new Dictionary<uint, string>()
+        {
+            { 0, "B1F East: North Chest" },
+            { 1, "B1F Main: A3 Top Chest" },
+            { 2, "B1F Clear Crystal Room: Chest" },
+            { 3, "B1F Main: A3 Middle Chest" },
+            { 4, "B1F Main: A3 Bottom Chest" },
+            { 5, "B1F East: Ragelope Top Chest" },
+            { 6, "B1F East: Ragelope Middle Chest" },
+            { 7, "B1F Violet Crystal Room: Top Chest"},
+            { 8, "B1F Violet Crystal Room: Bottom Chest"},
+            { 9 , "B1F East: Ragelope Bottom Chest" }
         };
 
 
@@ -18,5 +33,7 @@ namespace EOAP.Plugin
                 return string.Format("Shop: {0}", itemName);
             return string.Empty;
         }
+
+        public static bool TryGetTreasureBoxLocation(uint tboxID, out string location) => TreasureBox.TryGetValue(tboxID, out location);
     }
 }
