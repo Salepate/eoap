29edb067641607f3bfc14e6d92099f4d73718627
Salepate <salepate@gmail.com>
2025-11-24 00:24:21 +0100

    adapt location names to apworld

--- EOAP.Plugin/EO1.cs
+++ EOAP.Plugin/EO1.cs
@@ -14,23 +14,23 @@ namespace EOAP.Plugin
 
         public static readonly Dictionary<uint, string> TreasureBox = new Dictionary<uint, string>()
         {
-            { 0, "B1F East: North Chest" },
-            { 1, "B1F Main: A3 Top Chest" },
-            { 2, "B1F Clear Crystal Room: Chest" },
-            { 3, "B1F Main: A3 Middle Chest" },
-            { 4, "B1F Main: A3 Bottom Chest" },
-            { 5, "B1F East: Ragelope Top Chest" },
-            { 6, "B1F East: Ragelope Middle Chest" },
-            { 7, "B1F Violet Crystal Room: Top Chest"},
-            { 8, "B1F Violet Crystal Room: Bottom Chest"},
-            { 9 , "B1F East: Ragelope Bottom Chest" }
+            { 0, "B1F East - North Chest" },
+            { 1, "B1F Main - A3 Top Chest" },
+            { 2, "B1F Clear Crystal Room - Chest" },
+            { 3, "B1F Main - A3 Middle Chest" },
+            { 4, "B1F Main - A3 Bottom Chest" },
+            { 5, "B1F East - Ragelope Top Chest" },
+            { 6, "B1F East - Ragelope Middle Chest" },
+            { 7, "B1F Violet Crystal Room - Top Chest"},
+            { 8, "B1F Violet Crystal Room - Bottom Chest"},
+            { 9 , "B1F East - Ragelope Bottom Chest" }
         };
 
         // API
         public static string GetShopLocation(int itemID)
         {
             if (ItemIDToName.TryGetValue(itemID, out string itemName))
-                return string.Format("Shop: {0}", itemName);
+                return string.Format("Shop - {0}", itemName);
             return string.Empty;
         }
 
