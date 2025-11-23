c86f7e17e2d0fda1714417151080b58cca9492c7
Salepate <salepate@gmail.com>
2025-11-24 00:13:48 +0100

    restore items on game load

---
+++ EOAP.Plugin/Patcher/TownManager_OpenInn.cs
@@ -0,0 +1,25 @@
+﻿using EOAP.Plugin.AP;
+using EOAP.Plugin.Behaviours;
+using HarmonyLib;
+
+namespace EOAP.Plugin.Patcher
+{
+    // Seem to be invoked only once per save
+    [HarmonyPatch(typeof(TownManager), nameof(TownManager.OpenInn))]
+    public class AutoPatch_TownManager_OpenInn 
+    { 
+        public static void Prefix() 
+        {
+            var persistent = APBehaviour.GetPersistent();
+            var session = APBehaviour.GetSession();
+            if (session.Connected)
+            {
+                session.LoadFlags(persistent);
+            }
+            else
+            {
+                GDebug.Log("Not connected to AP, cannot restore items");
+            }
+        }
+    }
+}
