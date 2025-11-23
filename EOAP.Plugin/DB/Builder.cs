3b79ef4996a20accbd2ff09299b336ac9703b588
Salepate <salepate@gmail.com>
2025-11-24 00:10:40 +0100

    add dyndb builder
    used to dump some game data more easily

---
+++ EOAP.Plugin/DB/Builder.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EOAP.Plugin.DB
+{
+    public static class Builder
+    {
+        private static DynDB m_DB;
+        private  static JsonConverter m_Converter;
+        static Builder()
+        {
+            m_DB = new DynDB();
+        }
+
+        public static void Load(string path)
+        {
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                m_DB = JsonConvert.DeserializeObject<DynDB>(content);
+                GDebug.Log("loaded dyndb");
+            }
+            else
+            {
+                GDebug.Log("dyndb not found");
+            }
+        }
+        public static void Dump(string path)
+        {
+            m_DB.SortByID();
+            string jsonDB = JsonConvert.SerializeObject(m_DB, Formatting.Indented);
+            File.WriteAllText(path, jsonDB);
+            int entries = 0;
+            foreach(var tbl in m_DB.Tables)
+            {
+                entries += tbl.Value.Count;
+            }
+            GDebug.Log($"saved dyndb / tables : {m_DB.Tables.Count} / totale entries {entries}");
+        }
+
+        public static void Push(string db, int id, params string[] additionalData)
+        {
+            List<Entry> tbl = m_DB.GetTable(db);
+            if (!DynDB.GetEntry(tbl, id, out int tblIndex))
+            {
+                tbl.Add(new Entry() { ID = id, Data = additionalData });
+            }
+        }
+    }
+}
---
+++ EOAP.Plugin/DB/DynDB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using static Master.MasterManager;
+
+namespace EOAP.Plugin.DB
+{
+    [System.Serializable]
+    public class DynDB
+    {
+        public Dictionary<string, List<Entry>> Tables;
+
+        public DynDB()
+        {
+            Tables = new Dictionary<string, List<Entry>>();
+        }
+
+        public List<Entry> GetTable(string tbl)
+        {
+            if (!Tables.TryGetValue(tbl, out var entries))
+            {
+                entries = new List<Entry>();
+                Tables.Add(tbl, entries);
+            }
+            return entries;
+        }
+
+
+        public static bool GetEntry(in List<Entry> table, int entryID, out int tableIndex)
+        {
+            for(int i = 0; i < table.Count; ++i)
+            {
+                if (table[i].ID == entryID)
+                {
+                    tableIndex = i;
+                    return true;
+                }
+            }
+            tableIndex = -1;
+            return false;
+        }
+
+        internal void SortByID()
+        {
+            var oldTables = Tables;
+            Tables = new Dictionary<string, List<Entry>>();
+            foreach(var table in oldTables)
+            {
+                List<Entry> copyEntries = new List<Entry>();
+                copyEntries.AddRange(table.Value);
+                copyEntries.Sort((a, b) => a.ID - b.ID);
+                Tables.Add(table.Key, copyEntries);
+            }
+        }
+    }
+
+
+    [System.Serializable]
+    public struct Entry
+    {
+        public int ID;
+        public string[] Data;
+    }
+}
