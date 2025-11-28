5adab7cfb0a2f4e51f9d53ce104aef306fedd0af
Salepate <salepate@gmail.com>
2025-11-28 23:05:25 +0100

    connect starting gold feature

---
+++ EOAP.Plugin/Patcher/Feature_StartingGold.cs
@@ -0,0 +1,23 @@
+﻿using EOAP.Plugin.AP;
+using HarmonyLib;
+
+namespace EOAP.Plugin.Patcher
+{
+    [HarmonyPatch(typeof(SQSceneManager), nameof(SQSceneManager.LoadedEntranceOpeningEvent))]
+    internal class Feature_StartingGold
+    {
+        public static void Prefix()
+        {
+            int startGold = GoldItem.GetGold();
+
+            if (EOConfig.StartingEntal != 0 && startGold != EOConfig.StartingEntal)
+            {
+                int delta = EOConfig.StartingEntal - startGold;
+                if (delta > 0)
+                    GoldItem.GiveGold((uint)delta);
+                else if (delta < 0)
+                    GoldItem.PayGold_WithoutSEPlay((uint)(-delta));
+            }
+        }
+    }
+}
