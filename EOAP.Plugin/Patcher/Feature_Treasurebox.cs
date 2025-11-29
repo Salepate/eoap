6c8bed4176307c57806e8b181c75c76431bdeecb
Salepate <salepate@gmail.com>
2025-11-29 13:17:52 +0100

    skip reward if location

--- EOAP.Plugin/Patcher/Feature_Treasurebox.cs
+++ EOAP.Plugin/Patcher/Feature_Treasurebox.cs
@@ -6,13 +6,16 @@ namespace EOAP.Plugin.Patcher
     [HarmonyPatch(typeof(DungeonTreasureState), nameof(DungeonTreasureState.GetItemFunc))]
     public class DungeonTreasureState_GetItemFunc
     {
-        public static void Prefix(DungeonTreasureState __instance)
+        public static bool Prefix(DungeonTreasureState __instance)
         {
             DungeonData.TBData treasureBox = __instance.KOHPIHBMMIC;
             if (EO1.TryGetTreasureBoxLocation(treasureBox.TbId, out string loc))
             {
                 APBehaviour.GetSession().SendLocation(loc);
+                __instance.OnEnd();
+                return false;
             }
+            return true;
         }
     }
 }
