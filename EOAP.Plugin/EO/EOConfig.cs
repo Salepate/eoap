76494134a7d00979c1fd2cd718619eadab513e4c
Salepate <salepate@gmail.com>
2025-12-03 20:30:17 +0100

    add xp modifier config

--- EOAP.Plugin/EO/EOConfig.cs
+++ EOAP.Plugin/EO/EOConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using static ResultManager;
 
 namespace EOAP.Plugin.EO
 {
@@ -13,6 +14,7 @@ namespace EOAP.Plugin.EO
         public static int GoalMission = 0; //
         public static bool GoalTypeMission = true;
 
+        public static uint XPPercent; // experience modifier (%)
         public static int StartingEntal = 1000; // default values
         public static int PriceOverride = -1;
         public static int PriceScale = 100;
@@ -53,7 +55,8 @@ namespace EOAP.Plugin.EO
                 { "price_percent_value", (v) => PriceScale = (int)v },
                 { "price_flat_value", (v) => PriceOverride = (int)v },
                 { "goal_mode", (v) => GoalTypeMission = v == 0 },
-                { "goal_mission", (v) => GoalMission = (int)v }
+                { "goal_mission", (v) => GoalMission = (int)v },
+                { "exp_percent_value", (v) => XPPercent = (uint)v }
             };
         }
     }
---
+++ EOAP.Plugin/Patcher/Configuration/XPModifier.cs
@@ -0,0 +1,17 @@
+﻿using EOAP.Plugin.EO;
+using HarmonyLib;
+using Master;
+
+namespace EOAP.Plugin.Patcher.Configuration
+{
+    [HarmonyPatch(typeof(MasterManager), nameof(MasterManager.GetMasterEnemyData))]
+    public class XPModifier
+    {
+        public static void Postfix(ref MasterTbbData.ENEMY_DATA __result, int PFODICHNECH)
+        {
+            uint xpValue = __result.FDFEGNNAEJL;
+            xpValue = xpValue * EOConfig.XPPercent / 100;
+            __result.FDFEGNNAEJL = xpValue;
+        }
+    }
+}
